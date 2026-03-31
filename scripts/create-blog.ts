41de385ad3ed4e36d02f4ec36d6bfb5c7678092c
Joaquin Esteban <dev@joaco.io>
2026-03-31 02:49:44 -0300

    feat: add create-blog script

---
+++ scripts/create-blog.ts
@@ -0,0 +1,6 @@
+
+if (import.meta.main) await exec()
+
+export async function exec() {
+  await Bun.$`/opt/homebrew/bin/opencode run "new blog: @specs/blog-posts.md. this is an unassisted task. commit your changes when you finish"`
+}
